81c0668606405d8de5fcdfdebebed8e629046972
zion <0o0w0d2@gmail.com>
2023-11-30 03:33:03 +0900

    로그인 처리 - authenticate/login

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('feeds', views.feeds)
+    path('feeds/', views.feeds)
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
 def login_view(request):
+    # 로그인 상태일 시 redirect
     if request.user.is_authenticated:
-        return redirect('/posts/feeds')
+        return redirect('/posts/feeds/')
 
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        # POST된 data를 form에 넣은 객체 생성
+        form = LoginForm(data=request.POST)
+
+        # 유효성 검사
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # username, password에 해당하는 user 검사
+            user = authenticate(username=username, password=password)
+
+            # user가 존재할 때
+            if user:
+                # 로그인 상태로 변환 및 유지
+                login(request, user)
+                return redirect('/posts/feeds/')
+            # user가 없을 때(return 값이 없음)
+            else:
+                print('해당 사용자가 없습니다')
+
+        # 유효성 검사 실패 or user가 없을 때
+        context = {'form': form}
+        return render(request, 'users/login.html', context)
+
+    # request.method == GET
+    else :
+        form = LoginForm()
+        context = {'form': form}
+        return render(request, 'users/login.html', context)
