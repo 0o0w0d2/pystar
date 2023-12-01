984409ca13205375cf3de2595e3b6e8e717a32e5
zion <0o0w0d2@gmail.com>
2023-12-02 03:50:31 +0900

    피드 페이지 view / 작성자 표시

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from .models import Post, PostImage, Comment
 
 # Create your views here.
 def feeds(request):
     if not request.user.is_authenticated :
         return redirect('/users/login/')
 
-    return render(request, 'posts/feeds.html')
+    posts = Post.objects.all()
+
+    return render(request, 'posts/feeds.html', {'posts' : posts})
