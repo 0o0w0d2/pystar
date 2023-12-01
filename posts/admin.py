c1bf519504112cc7e0052a90006eb4f898e75187
zion <0o0w0d2@gmail.com>
2023-12-02 03:10:30 +0900

    admin을 조작해서 썸네일 표시

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,7 @@
+from django.contrib.admin.widgets import AdminFileWidget
+from django.db import models
+from django.utils.safestring import mark_safe
+
 from django.contrib import admin
 from .models import Post, PostImage, Comment
 
@@ -5,9 +9,26 @@ class CommentInline(admin.TabularInline):
     model = Comment
     extra = 1
 
+# admin을 조작해서 썸네일 표시
+class InlineImageWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None, renderer=None):
+        html = super().render(name, value, attrs, renderer)
+        if value and getattr(value, 'url', None):
+            html = mark_safe(f'<img src="{value.url}" height="150">' + html)
+        return html
+
 class PostImageInline(admin.TabularInline):
     model = PostImage
     extra = 1
+    # admin을 조작해서 썸네일 표시할 때 추가해야할 코드
+    formfield_overrides = {
+        models.ImageField: {
+            'widget': InlineImageWidget
+        }
+    }
+
+
+
 
 # Register your models here.
 @admin.register(Post)
