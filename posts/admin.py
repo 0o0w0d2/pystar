4e86c7726addc29854056f0eaed4d2df70eab695
zion <0o0w0d2@gmail.com>
2023-12-02 02:57:09 +0900

    Post Model + admin 구현

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Post, PostImage, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'content'
+    ]
+    inlines = [
+        CommentInline,
+        PostImageInline
+    ]
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'post', 'photo'
+    ]
+
+
+@admin.register(Comment)
+class Comment(admin.ModelAdmin):
+    list_display = [
+        'id', 'post', 'content'
+    ]
