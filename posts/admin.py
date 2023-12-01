bba653e04f3e9af13f94aa501454b25d44978741
zion <0o0w0d2@gmail.com>
2023-12-02 03:14:21 +0900

    썸네일 외부 라이브러리 사용으로 변경 ( django-admin-thumbnails )

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,9 @@
-from django.contrib.admin.widgets import AdminFileWidget
-from django.db import models
-from django.utils.safestring import mark_safe
+# admin을 조작해서 썸네일 표시할 때 필요한 모듈
+# from django.contrib.admin.widgets import AdminFileWidget
+# from django.db import models
+# from django.utils.safestring import mark_safe
 
+import admin_thumbnails
 from django.contrib import admin
 from .models import Post, PostImage, Comment
 
@@ -10,22 +12,23 @@ class CommentInline(admin.TabularInline):
     extra = 1
 
 # admin을 조작해서 썸네일 표시
-class InlineImageWidget(AdminFileWidget):
-    def render(self, name, value, attrs=None, renderer=None):
-        html = super().render(name, value, attrs, renderer)
-        if value and getattr(value, 'url', None):
-            html = mark_safe(f'<img src="{value.url}" height="150">' + html)
-        return html
+# class InlineImageWidget(AdminFileWidget):
+#     def render(self, name, value, attrs=None, renderer=None):
+#         html = super().render(name, value, attrs, renderer)
+#         if value and getattr(value, 'url', None):
+#             html = mark_safe(f'<img src="{value.url}" height="150">' + html)
+#         return html
 
+@admin_thumbnails.thumbnail('photo')
 class PostImageInline(admin.TabularInline):
     model = PostImage
     extra = 1
     # admin을 조작해서 썸네일 표시할 때 추가해야할 코드
-    formfield_overrides = {
-        models.ImageField: {
-            'widget': InlineImageWidget
-        }
-    }
+    # formfield_overrides = {
+    #     models.ImageField: {
+    #         'widget': InlineImageWidget
+    #     }
+    # }
 
 
 
