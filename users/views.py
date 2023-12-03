f905eb9a33a9fc1f04ad6b27594fcb0419248f95
zion <0o0w0d2@gmail.com>
2023-12-04 01:13:13 +0900

    동적 URL(View) reverse 함수 사용 + redirect의 경우 reverse 자동으로 적용

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from .models import User
 def login_view(request):
     # 로그인 상태일 시 redirect
     if request.user.is_authenticated:
-        return redirect('/posts/feeds/')
+        return redirect('posts:feeds')
 
     if request.method == "POST":
         # POST된 data를 form에 넣은 객체 생성
@@ -25,7 +25,7 @@ def login_view(request):
             if user:
                 # 로그인 상태로 변환 및 유지
                 login(request, user)
-                return redirect('/posts/feeds/')
+                return redirect('posts:feeds')
             # user가 없을 때(return 값이 없음)
             else:
                 form.add_error(None, '입력한 정보를 다시 확인해주세요.')
@@ -44,7 +44,7 @@ def login_view(request):
 def logout_view(request):
 
     logout(request)
-    return redirect('/users/login/')
+    return redirect('users:login')
 
 
 def signup(request):
@@ -54,7 +54,7 @@ def signup(request):
         # 유효성 검사
         if form.is_valid():
             form.save()
-            return redirect('/users/login/')
+            return redirect('users:login')
 
     # GET 요청
     else :
