dceae88009f8dcaae3da29051e123e1234a31508
zion <0o0w0d2@gmail.com>
2023-12-08 00:45:22 +0900

    좋아요 기능 - model(users -> ManyToManyField 로 추가) - view(posts -> post_like) - template(posts -> post.html)

--- users/admin.py
+++ users/admin.py
@@ -10,5 +10,6 @@ class CustomUserAdmin(UserAdmin):
         ('개인 정보', {'fields': ('first_name', 'last_name', 'email')}),
         ('추가 필드', {'fields': ('profile_image', 'short_description')}),
         ('권한', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        ('중요한 일정', {'fields':('last_login', 'date_joined')})
+        ('중요한 일정', {'fields':('last_login', 'date_joined')}),
+        ('연관 객체', {'fields': ('like_posts',)})
     ]
--- users/models.py
+++ users/models.py
@@ -7,3 +7,12 @@ class User(AbstractUser):
     short_description = models.TextField("간단 소개", blank=True)
     profile_image = models.ImageField(
         "프로필 사진", upload_to="users/profile", blank=True)
+    like_posts = models.ManyToManyField(
+        "posts.Post",
+        verbose_name='좋아요 누른 Post 목록',
+        related_name='like_users',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
